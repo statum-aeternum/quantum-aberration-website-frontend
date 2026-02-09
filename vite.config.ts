3459e17fe6fd23c6c3521431317a1d7e1a2d0041
Statum <statum.music@gmail.com>
2026-02-09 15:44:37 +0100

    feat: add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -13,7 +14,13 @@ export default ({ mode }: { mode: string }) => {
   }
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      Sitemap({
+        hostname: "https://example.com",
+        dynamicRoutes: ["/", "/about", "/news", "/merch", "/contact"],
+      }),
+    ],
     server: {
       watch: {
         usePolling: true,
